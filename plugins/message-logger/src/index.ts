04bd89145ecfe9322cb9c7ddea7ba9a9282bfa74
qqqyyyhhh8-del <qqqyyyhhh8@gmail.com>
2026-03-21 00:20:04 +0800

    Persist deleted messages in message logger

--- plugins/message-logger/src/index.ts
+++ plugins/message-logger/src/index.ts
@@ -1,18 +1,87 @@
 import { findByName, findByProps } from "@vendetta/metro";
 import { FluxDispatcher, ReactNative } from "@vendetta/metro/common";
 import { after, before, instead } from "@vendetta/patcher";
+import { storage } from "@vendetta/plugin";
 
 const patches = [];
 const ChannelMessages = findByProps("_channelMessages");
 const MessageRecordUtils = findByProps("updateMessageRecord", "createMessageRecord");
 const MessageRecord = findByName("MessageRecord", false);
 const RowManager = findByName("RowManager");
+const LOAD_EVENTS = new Set([
+  "LOAD_MESSAGES_AROUND_SUCCESS",
+  "LOAD_MESSAGES_SUCCESS",
+  "LOAD_MESSAGES_SUCCESS_CACHED",
+]);
 
-import { storage } from "@vendetta/plugin";
+storage.nopk ??= false;
+storage.deletedMessages ??= {};
+
+const deletedMessages = () => (storage.deletedMessages ??= {});
+
+const getStoredChannel = (channelId) => {
+  if (!channelId) return;
+  return deletedMessages()[channelId] ??= {};
+};
+
+const removeStoredMessage = (channelId, messageId) => {
+  const channel = deletedMessages()[channelId];
+  if (!channel) return;
+
+  delete channel[messageId];
+  if (!Object.keys(channel).length) delete deletedMessages()[channelId];
+};
+
+const serializeMessage = (message) => ({
+  ...(typeof message.toJS === "function" ? message.toJS() : message),
+  __vml_deleted: true,
+  __vml_savedAt: Date.now(),
+});
+
+const getCachedChannel = (channelId) => ChannelMessages.get(channelId) ?? ChannelMessages._channelMessages?.[channelId];
+
+const hasMessage = (channel, messageId) => {
+  if (!channel) return false;
+  if (typeof channel.get === "function") return !!channel.get(messageId);
+  if (channel._map?.[messageId]) return true;
+  return channel._array?.some((message) => message.id === messageId) ?? false;
+};
+
+const getMessageSortValue = (message) => {
+  const value = new Date(message.timestamp ?? 0).getTime();
+  return Number.isFinite(value) ? value : 0;
+};
+
+const restoreChannel = (channelId) => {
+  const stored = deletedMessages()[channelId];
+  const channel = getCachedChannel(channelId);
+  if (!stored || !channel) return;
+
+  const seen = new Set(channel._array?.map((message) => message.id) ?? []);
+
+  for (const message of Object.values(stored).sort((a, b) => getMessageSortValue(a) - getMessageSortValue(b))) {
+    if (seen.has(message.id) || hasMessage(channel, message.id)) continue;
+
+    seen.add(message.id);
+    FluxDispatcher.dispatch({
+      type: "MESSAGE_CREATE",
+      channelId,
+      message: {
+        ...message,
+        __vml_deleted: true,
+      },
+      optimistic: false,
+      __vml_restore: true,
+    });
+  }
+};
 
 patches.push(before("dispatch", FluxDispatcher, ([event]) => {
   if (event.type === "MESSAGE_DELETE") {
-    if (event.__vml_cleanup) return event;
+    if (event.__vml_cleanup) {
+      if (event.__vml_forget) removeStoredMessage(event.channelId, event.id);
+      return event;
+    }
 
     const channel = ChannelMessages.get(event.channelId);
     const message = channel?.get(event.id);
@@ -20,6 +89,9 @@ patches.push(before("dispatch", FluxDispatcher, ([event]) => {
 
     if (message.author?.id == "1") return event;
     if (message.state == "SEND_FAILED") return event;
+
+    const deletedMessage = serializeMessage(message);
+    getStoredChannel(message.channel_id)[message.id] = deletedMessage;
 
     storage.nopk && fetch(`https://api.pluralkit.me/v2/messages/${encodeURIComponent(message.id)}`)
       .then((res) => res.json())
@@ -30,18 +102,26 @@ patches.push(before("dispatch", FluxDispatcher, ([event]) => {
             id: message.id,
             channelId: message.channel_id,
             __vml_cleanup: true,
+            __vml_forget: true,
           });
         }
-      });
+      })
+      .catch(() => {});
 
     return [{
-      message: {
-        ...message.toJS(),
-        __vml_deleted: true,
-      },
+      message: deletedMessage,
       type: "MESSAGE_UPDATE",
     }];
   }
+}));
+
+patches.push(after("dispatch", FluxDispatcher, ([event]) => {
+  if (!LOAD_EVENTS.has(event.type)) return;
+
+  const channelId = event.channelId ?? event.messages?.[0]?.channel_id;
+  if (!channelId) return;
+
+  setTimeout(() => restoreChannel(channelId), 0);
 }));
 
 patches.push(after("generate", RowManager.prototype, ([data], row) => {
@@ -70,6 +150,14 @@ patches.push(after("default", MessageRecord, ([props], record) => {
   record.__vml_deleted = !!props.__vml_deleted;
   // record.__vml_edits = props.__vml_edits;
 }));
+
+export const onLoad = () => {
+  deletedMessages();
+
+  for (const channelId in ChannelMessages._channelMessages ?? {}) {
+    restoreChannel(channelId);
+  }
+};
 
 export const onUnload = () => {
   patches.forEach((unpatch) => unpatch());
